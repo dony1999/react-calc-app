087f33b814f2b229b1d88c52284b829305643145
dat-dony <102413999+dony1999@users.noreply.github.com>
2022-09-13 10:26:26 +0700

    update responsive

--- src/App.js
+++ src/App.js
@@ -1,100 +1,106 @@
-import './App.css';
-import { useState , useEffect} from 'react';
-import { NumericFormat } from 'react-number-format';
+import "./App.css";
+import "./Responsive.css"
+import { useState, useEffect } from "react";
+import { NumericFormat } from "react-number-format";
 
 function App() {
+  const [preState, setPreState] = useState("");
+  const [curState, setCurState] = useState("");
+  const [input, setInput] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [total, setTotal] = useState(false);
+  
+  console.log("-----------render------------");
+  console.log(preState + "preState:");
+  console.log("curState: " + curState);
+  console.log("input: " + input);
+  console.log("operator: " + operator);
+  console.log("total: " + total);
 
-    const [ preState, setPreState] = useState('')
-    const [ curState, setCurState] = useState('')
-    const [ input, setInput] = useState('0')
-    const [ operator, setOperator ] = useState(null)
-    const [ total, setTotal ] = useState(false)
+  const inputNumber = (e) => {
+    if (curState.includes(".") && e.target.innerText === ".") return;
+    if (total) {
+      setPreState("");
+    }
 
+    curState
+      ? setCurState((pre) => pre + e.target.innerText)
+      : setCurState(e.target.innerText);
+    setTotal(false);
+  };
 
-    const inputNumber = (e) => {
-      if (curState.includes('.') && e.target.innerText === ".")
-        return 
-      if (total) {
-        setPreState('')
-      }
-
-      curState ? setCurState((pre) => pre + e.target.innerText) : setCurState(e.target.innerText);
-      setTotal(false)
-    }  
-    
     useEffect(() => {
-      setInput(curState) 
-    }, [curState])
+      setInput(curState);
+    }, [curState]);
 
     useEffect(() => {
-      setInput('0')
-    }, [])
+      setInput("0");
+    },[]);
 
-    const operatorType = (e) => {
-      setTotal(false)
-      setOperator(e.target.innerText)
-      if(curState === "") return ;
-      if(preState !== "") {
-        equals();
-      } else
-      setPreState(curState)
-      setCurState('')
+  const operatorType = (e) => {
+    setTotal(false);
+    setOperator(e.target.innerText);
+    if (curState === "") return;
+    if (preState !== "") {
+      equals();
+    } else setPreState(curState);
+    setCurState("");
+  };
+
+  const equals = (e) => {
+    if (e?.target.innerText === "=") {
+      setTotal(true);
     }
 
-    const equals = (e) => {
-      if (e?.target.innerText === "="){
-        setTotal(true)
-      }
-    
-      var calculate 
-      switch(operator) {
-        case "+":
-          calculate = parseFloat(preState) + parseFloat(curState);
-          break;
-        case "-":
-          calculate = parseFloat(preState) - parseFloat(curState);
-          break;
-        case "X":
-          calculate = parseFloat(preState) * parseFloat(curState);
-          break;
-        case "÷": 
-          calculate = parseFloat(preState) / parseFloat(curState);
-          break;
-        default:
-          break;
-      }
-        setInput('')
-        setPreState(calculate)
-        setCurState('')
-    };
-
-    const minusPlus = () => {
-      if(curState.charAt(0) === "-"){
-        setCurState(curState.substring(1))
-      } else {
-        setCurState("-" + curState)
-      }
+    var calculate;
+    switch (operator) {
+      case "+":
+        calculate = parseFloat(preState) + parseFloat(curState);
+        break;
+      case "-":
+        calculate = parseFloat(preState) - parseFloat(curState);
+        break;
+      case "X":
+        calculate = parseFloat(preState) * parseFloat(curState);
+        break;
+      case "÷":
+        calculate = parseFloat(preState) / parseFloat(curState);
+        break;
+      default:
+        break;
     }
+    setInput('');
+    setPreState(calculate);
+    setCurState("");
+  };
 
-    const percent = () => {
-      if(preState) {
-        setCurState((parseFloat(curState) / 100) * 100)
-      } else {
-        setCurState(parseFloat(curState) / 100)
-      }
+  const minusPlus = () => {
+    if (curState.charAt(0) === "-") {
+      setCurState(curState.substring(1));
+    } else {
+      setCurState("-" + curState);
     }
+  };
 
-    const reset = () => {
-      setPreState('')
-      setCurState('')
-      setInput('0')
+  const percent = () => {
+    if (preState) {
+      setCurState((parseFloat(curState) / 100) * 100);
+    } else {
+      setCurState(parseFloat(curState) / 100);
     }
+  };
 
-    return (
+  const reset = () => {
+    setPreState("");
+    setCurState("");
+    setInput("0");
+  };
+
+  return (
     <div className="container">
-      <div className='wrapper'>
-        <div className='screen'>
-          { input !== "" || input === "0" ? (
+      <div className="wrapper">
+        <div className="screen">
+          {input !== "" || input === "0" ? (
             <NumericFormat
               value={input}
               displayType={"text"}
@@ -108,28 +114,70 @@ function App() {
             />
           )}
         </div>
-        <div className='btn btn-fnc' onClick={reset}>AC</div>
-        <div className='btn btn-fnc' onClick={minusPlus}>+/-</div>
-        <div className='btn btn-fnc' onClick={percent}>%</div>
-        <div className='btn btn-operator' onClick={operatorType}>÷</div>
-        <div className='btn number' onClick={inputNumber}>7</div>
-        <div className='btn number' onClick={inputNumber}>8</div>
-        <div className='btn number' onClick={inputNumber}>9</div>
-        <div className='btn btn-operator' onClick={operatorType}>X</div>
-        <div className='btn number' onClick={inputNumber}>4</div>
-        <div className='btn number' onClick={inputNumber}>5</div>
-        <div className='btn number' onClick={inputNumber}>6</div>
-        <div className='btn btn-operator' onClick={operatorType}>-</div>
-        <div className='btn number' onClick={inputNumber}>1</div>
-        <div className='btn number' onClick={inputNumber}>2</div>
-        <div className='btn number' onClick={inputNumber}>3</div>
-        <div className='btn btn-operator' onClick={operatorType}>+</div>
-        <div className='btn number zero' onClick={inputNumber}>0</div>
-        <div className='btn number' onClick={inputNumber}>.</div>
-        <div className='btn btn-operator' onClick={equals}> = </div>
+        <div className="btn-container">
+          <div className="btn btn-fnc" onClick={reset}>
+            AC
+          </div>
+          <div className="btn btn-fnc" onClick={minusPlus}>
+            +/-
+          </div>
+          <div className="btn btn-fnc" onClick={percent}>
+            %
+          </div>
+          <div className="btn btn-operator" onClick={operatorType}>
+            ÷
+          </div>
+          <div className="btn number" onClick={inputNumber}>
+            7
+          </div>
+          <div className="btn number" onClick={inputNumber}>
+            8
+          </div>
+          <div className="btn number" onClick={inputNumber}>
+            9
+          </div>
+          <div className="btn btn-operator" onClick={operatorType}>
+            X
+          </div>
+          <div className="btn number" onClick={inputNumber}>
+            4
+          </div>
+          <div className="btn number" onClick={inputNumber}>
+            5
+          </div>
+          <div className="btn number" onClick={inputNumber}>
+            6
+          </div>
+          <div className="btn btn-operator" onClick={operatorType}>
+            -
+          </div>
+          <div className="btn number" onClick={inputNumber}>
+            1
+          </div>
+          <div className="btn number" onClick={inputNumber}>
+            2
+          </div>
+          <div className="btn number" onClick={inputNumber}>
+            3
+          </div>
+          <div className="btn btn-operator" onClick={operatorType}>
+            +
+          </div>
+          <div className="btn number zero" onClick={inputNumber}>
+            0
+          </div>
+          <div className="btn number" onClick={inputNumber}>
+            .
+          </div>
+          <div className="btn btn-operator" onClick={equals}>
+            {" "}
+            ={" "}
+          </div>
+        </div>
       </div>
     </div>
-  )
+  
+  );
 }
 
 export default App;
